d3ea958362f08e9f956cf56c68a129205700ee0f
Mattia46 <mattia.assogna@gmail.com>
2019-09-27 10:33:02 +0100

    added s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,29 @@ provider "aws" {
   region              = "us-east-1"
   allowed_account_ids = ["621041455573"] # dazn-demo-one
 }
+
+resource "aws_s3_bucket" "b" {
+  bucket = "mattia-bucket"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_object" "main-obj" {
+    bucket = var.main
+    key    = var.main
+    source = var.main_path
+    etag   = "${filemd5(var.main_path)}"
+}
+
+resource "aws_s3_bucket_object" "second-obj" {
+    bucket = var.second
+    key    = var.second
+    source = var.second_path
+    etag   = "${filemd5(var.second_path)}"
+}
+
+resource "aws_s3_bucket_object" "style-obj" {
+    bucket = var.style
+    key    = var.style
+    source = var.style_path
+    etag   = "${filemd5(var.style_path)}"
+}
---
+++ terraform/variable.tf
@@ -0,0 +1,29 @@
+variable "main_path" {
+    description = "main html path"
+    default     = "../public/main.html"
+}
+
+variable "main" {
+    description = "main html file"
+    default     = "main.html"
+}
+
+variable "second_path" {
+    description = "second html path"
+    default     = "../public/second.html"
+}
+
+variable "second" {
+    description = "second html path"
+    default     = "second.html"
+}
+
+variable "style_path" {
+    description = "style folder path"
+    default     = "../public/static/style.css"
+}
+
+variable "style" {
+    description = "style.css file"
+    default     = "style.css"
+}
